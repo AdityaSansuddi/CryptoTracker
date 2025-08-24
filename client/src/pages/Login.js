b10de020091f79540dba2b82ff6f8c95d2378f2a
AdityaSansuddi <adisansuddi@gmail.com>
2025-08-24 13:30:32 +0530

    Replaced localhost

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { API_BASE_URL } from "../api";
 import toast from "react-hot-toast";
-import { API_BASE_URL } from "../api";
 
 
 
